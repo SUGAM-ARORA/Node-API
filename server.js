c55d4f5dcf1821d8849c064183f641cfa04a645f
Sugam <sugam.arora23@gmail.com>
2023-07-28 19:16:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('Hello');
+const express=require('express');
+const app=express()
+
+app.get('/'),(req,res)=>{
+    res.send('Hello Node API')
+}
+
+app.listen(3000,()=>{
+    console.log('Node API is running on port 3000')
+})
